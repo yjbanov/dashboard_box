69f3144993460a5918c3411c148938b0df2d5787
Yegor Jbanov <yjbanov@google.com>
2016-04-06 10:36:31 -0700

    a dashboard generated by Jekyll

--- README.md
+++ README.md
@@ -5,17 +5,18 @@ dashboard.
 
 To test this script locally:
 
-- Get a Mac with a recent Mac OS X
+- Get a Mac with a recent Mac OS X.
 - Clone this repository _exactly_ into `~/flutter_dashboard/dashboard_box`, so
   that the `run.sh` script could be found _exactly_ at
-  `~/flutter_dashboard/dashboard_box/run.sh`
-- For better results, make sure there's nothing else in `~/flutter_dashboard`
+  `~/flutter_dashboard/dashboard_box/run.sh`.
+- For better results, make sure there's nothing else in `~/flutter_dashboard`.
 - Complete the [Flutter Setup](https://flutter.io/setup/), if you haven't
   already. Note, however, that this job will install its own copy of Flutter.
-- Connect an Android device
+- Install [Jekyll](https://jekyllrb.com).
+- Connect an Android device.
 - Set `ANDROID_DEVICE_ID` environment variable to the ID of your device as
   reported by `flutter devices` command (this is because the ID of the device
-  connected to the box is not the same as yours)
+  connected to the box is not the same as yours).
 - Launch `run.sh`. Or, if you are testing the `lanuchd` configuration, then
   `launchctl start job.plist`, but don't forget to kill the job after testing,
   otherwise it will spin on your computer forever, draining all kinds of
--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 set -e
 set -x
 
+function absolute_path {
+  [[ $1 = /* ]] && echo "$1" || echo "$(pwd)/${1#./}"
+}
+
 echo "-----------------------------------------"
 echo "Build started on $(date)"
 echo "-----------------------------------------"
@@ -10,13 +14,32 @@ echo "-----------------------------------------"
 # externally
 export ANDROID_DEVICE_ID=${ANDROID_DEVICE_ID:-"AG860440G62GIGC"}
 
-ROOT_DIRECTORY=$(dirname $(dirname $0))
+ROOT_DIRECTORY=$(absolute_path "$(dirname $(dirname $0))")
 DASHBOARD_DIRECTORY="$ROOT_DIRECTORY/dashboard"
+JEKYLL_DIRECTORY="$ROOT_DIRECTORY/dashboard_box/jekyll"
+DATA_DIRECTORY="$JEKYLL_DIRECTORY/_data"
 FLUTTER_DIRECTORY="$ROOT_DIRECTORY/flutter"
+SCRIPTS_DIRECTORY="$ROOT_DIRECTORY/dashboard_box"
 
+BUILD_INFO_FILE="$DATA_DIRECTORY/build.json"
+SUMMARIES_FILE="$DATA_DIRECTORY/summaries.json"
+
+echo "ROOT_DIRECTORY: $ROOT_DIRECTORY"
 echo "DASHBOARD_DIRECTORY: $DASHBOARD_DIRECTORY"
+echo "DATA_DIRECTORY: $DATA_DIRECTORY"
 echo "FLUTTER_DIRECTORY: $FLUTTER_DIRECTORY"
 echo "ANDROID_DEVICE_ID: $ANDROID_DEVICE_ID"
+echo "Jekyll version: $(jekyll --version)"
+
+if [[ -d $DATA_DIRECTORY ]]; then
+  rm -rf $DATA_DIRECTORY
+fi
+mkdir -p $DATA_DIRECTORY
+
+
+# --------------------------
+# Get Flutter
+# --------------------------
 
 cd $ROOT_DIRECTORY
 rm -rf $FLUTTER_DIRECTORY
@@ -25,12 +48,59 @@ git clone https://github.com/flutter/flutter.git
 export PATH=$(pwd)/flutter/bin:$PATH
 export PATH=$(pwd)/flutter/bin/cache/dart-sdk/bin:$PATH
 
-cd $FLUTTER_DIRECTORY
 flutter doctor
 
-cd examples/stocks
-pub get 1>&2
-flutter drive --verbose --no-checked --target=test_driver/scroll_perf.dart --device-id=$ANDROID_DEVICE_ID
+
+# --------------------------
+# Run tests
+# --------------------------
+
+function runTest {
+  TEST_DIRECTORY=$1
+  TEST_TARGET=$2
+
+  cd $TEST_DIRECTORY
+  pub get 1>&2
+  flutter drive --verbose --no-checked --target=$TEST_TARGET --device-id=$ANDROID_DEVICE_ID
+  cp build/*.timeline_summary.json $DATA_DIRECTORY
+}
+
+runTest $FLUTTER_DIRECTORY/examples/stocks test_driver/scroll_perf.dart
+runTest $FLUTTER_DIRECTORY/dev/benchmarks/complex_layout test_driver/scroll_perf.dart
+
+
+# --------------------------
+# Generate dashboard
+# --------------------------
+
+cd $DASHBOARD_DIRECTORY
+
+SUMMARIES="{"
+for jsonFile in $(ls $DATA_DIRECTORY/*.timeline_summary.json); do
+  NAME="${jsonFile%.*}"
+  NAME="${NAME%.*}"
+  SUMMARIES="${SUMMARIES} \"$(basename $NAME)\": "
+  SUMMARIES="${SUMMARIES} $(cat $jsonFile)"
+  SUMMARIES="${SUMMARIES},"
+done
+SUMMARIES="${SUMMARIES} \"blank\": {}}"
+echo $SUMMARIES > $SUMMARIES_FILE
+
+echo "{" > ${BUILD_INFO_FILE}
+echo "\"build_timestamp\": \"$(date)\"" >> ${BUILD_INFO_FILE}
+echo "}" >> ${BUILD_INFO_FILE}
+
+if [[ -d tmp ]]; then
+  rm -rf tmp
+fi
+jekyll build --source $JEKYLL_DIRECTORY --destination tmp/
+
+if [[ -d current ]]; then
+  TIMESTAMP=$(date +"%y-%m-%d-%H%M%S")
+  mv current $TIMESTAMP
+fi
+
+mv tmp current
 
 echo "-----------------------------------------"
 echo "Build finished on $(date)"
