67dd6b23565b5bcf785a1d9a6a62943975a3d683
Seth Ladd <sethladd@gmail.com>
2016-05-05 15:50:43 -0700

    travis ci (#28)
    * travis ci
    
    * make it executable

---
+++ tool/travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# TODO: deploy to a personal staging site, based on github ID, when not
+#       merging into master
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  if [ "$TRAVIS_BRANCH" = "master" ]; then
+    echo "Deploying to Firebase."
+
+    npm install -g firebase-tools
+    firebase deploy --token "$FIREBASE_TOKEN" -f purple-butterfly-3000
+  fi
+fi
