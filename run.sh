9d5adfd688f2393e3101bfeb5db15f609c1a9bff
Yegor <yjbanov@google.com>
2016-05-06 12:51:34 -0700

    use brew-installed Dart SDK for the build script

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ function absolute_path {
   [[ $1 = /* ]] && echo "$1" || echo "$(pwd)/${1#./}"
 }
 
+DART_SDK=${DART_SDK:-"/usr/local/opt/dart/libexec"}
 ROOT_DIRECTORY=$(dirname "$(dirname $(absolute_path "$0"))")
 
 cd ${ROOT_DIRECTORY}/dashboard_box
-pub get
-dart bin/build.dart $ROOT_DIRECTORY
+$DART_SDK/bin/pub get
+$DART_SDK/bin/dart bin/build.dart $ROOT_DIRECTORY
