8536a404313068b6165fd65c9fa755236d5e7c86
Yegor <yjbanov@google.com>
2016-05-09 17:35:01 -0700

    auto-update dashboard code (#36)

--- run.sh
+++ run.sh
@@ -2,12 +2,7 @@
 set -e
 set -x
 
-function absolute_path {
-  [[ $1 = /* ]] && echo "$1" || echo "$(pwd)/${1#./}"
-}
-
 DART_SDK=${DART_SDK:-"/usr/local/opt/dart/libexec"}
-ROOT_DIRECTORY=$(dirname "$(dirname $(absolute_path "$0"))")
 
 cd ${ROOT_DIRECTORY}/dashboard_box
 $DART_SDK/bin/pub get
