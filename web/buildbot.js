a0eee3831c81306e73fd4c0ec2281309940bba1e
Seth Ladd <sethladd@gmail.com>
2016-05-11 18:40:49 -0700

    show buildbot status (#46)
    * show buildbot status
    
    * handle both builders

---
+++ web/buildbot.js
@@ -0,0 +1,46 @@
+(function() {
+  const url = 'https://build.chromium.org/p/client.flutter/json/builders/';
+
+  function getBuildStatus(builderName) {
+    var urlWithBuilder = url + builderName + '/';
+
+    fetch(urlWithBuilder+'builds').then(function(response){
+      if (response.status !== 200) {
+        console.error('Error status listing builds: ' + response.status);
+        return Promise.reject(new Error(response.statusText));
+      }
+
+      return response.json();
+    }).then(function(data) {
+      var keys = Object.keys(data);
+      var latest = keys[keys.length-1];
+      return Promise.resolve(latest);
+    }).then(function(latestBuildNum) {
+      return Promise.resolve(fetch(urlWithBuilder+'builds/'+latestBuildNum));
+    }).then(function(response) {
+      if (response.status !== 200) {
+        console.error('Error status retrieving build info: ' + response.status);
+        return Promise.reject(new Error(response.statusText));
+      }
+
+      return response.json();
+    }).then(function(data) {
+      var isSuccessful = data['text'] && data['text'][1] === 'successful';
+      var elem = document.querySelector('#buildbot-' + builderName.toLowerCase() + '-status');
+      if (isSuccessful) {
+        elem.classList.remove('buildbot-sad');
+        elem.classList.add('buildbot-happy');
+      } else {
+        elem.classList.remove('buildbot-happy');
+        elem.classList.add('buildbot-sad');
+      }
+    }).catch(function(err) {
+      console.error(err);
+    });
+
+    setTimeout(getBuildStatus, 5*60*1000);
+  }
+
+  getBuildStatus('Mac');
+  getBuildStatus('Linux');
+})();
