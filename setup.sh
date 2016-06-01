4a0c4610623277ec7f2e5160a8713a9b9df62093
Yegor Jbanov <yjbanov@google.com>
2016-06-01 11:41:25 -0700

    fix logging

--- setup.sh
+++ setup.sh
@@ -17,12 +17,14 @@ ROOT_DIRECTORY=$(dirname "$(dirname $(absolute_path "$0"))")
 SCRIPT_DIRECTORY=${ROOT_DIRECTORY}/dashboard_box
 GSUTIL=${GSUTIL:-"/Users/$USER/google-cloud-sdk/bin/gsutil"}
 
+LOG_FILE="/tmp/flutter.dashboard.output.txt"
+echo 'Pulling from git' >$LOG_FILE
+
 (cd $SCRIPT_DIRECTORY; git pull)
 
-rm -f /tmp/flutter.dashboard.output.txt
-
+echo 'Starting run.sh' >>$LOG_FILE
 set +e
-(cd $ROOT_DIRECTORY; ROOT_DIRECTORY=$ROOT_DIRECTORY $SCRIPT_DIRECTORY/run.sh)
+(cd $ROOT_DIRECTORY; ROOT_DIRECTORY=$ROOT_DIRECTORY $SCRIPT_DIRECTORY/run.sh 2>&1 >>$LOG_FILE)
 set -e
 
 # Upload logs to Google Cloud Storage
@@ -31,5 +33,5 @@ pushd $ROOT_DIRECTORY/flutter
 SHA=$(git rev-parse HEAD)
 popd
 
-$GSUTIL cp /tmp/flutter.dashboard.output.txt gs://flutter-dashboard/$SHA/output.txt
+$GSUTIL cp $LOG_FILE gs://flutter-dashboard/$SHA/output.txt
 $GSUTIL -m acl ch -R -g 'google.com:R' gs://flutter-dashboard/$SHA
