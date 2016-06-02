cb60f7b415c14fa40cf7ffb0e3b1e58a09f4cbdc
Yegor Jbanov <yjbanov@google.com>
2016-06-02 10:57:29 -0700

    kill any dangling processes between runs and recycle adb

--- run.sh
+++ run.sh
@@ -2,8 +2,25 @@
 set -e
 set -x
 
+function clean_up {
+  pushd $ROOT_DIRECTORY
+  ABSOLUTE_ROOT_DIRECTORY=`pwd`
+
+  set +e
+  killall adb
+  set -e
+  ps -e -o pid,command | grep $ABSOLUTE_ROOT_DIRECTORY | grep -v grep | cut -d' ' -f1 | xargs kill
+
+  popd
+}
+
 DART_SDK=${DART_SDK:-"/usr/local/opt/dart/libexec"}
 
+clean_up
+
+# Run
 cd ${ROOT_DIRECTORY}/dashboard_box
 $DART_SDK/bin/pub get
 $DART_SDK/bin/dart bin/build.dart $ROOT_DIRECTORY
+
+clean_up
